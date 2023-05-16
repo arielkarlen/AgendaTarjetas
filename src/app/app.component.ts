ad894f408527d6fc8aa2217a82b23e22653830c0
arie.karlen <ariel.karlen@possumus.tech>
2023-05-16 10:01:59 -0300

    llamada a api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CardsService } from './services/tarjeta.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AgendaTarjetas';
+ 
+  cardList: any[]=[]
+
+constructor(private  _apicards: CardsService){}
+
+ngOnInit(): void {
+
+  
+  // this.listarTarjetas()
+  
+}
+
+  // listarTarjetas(){
+  //   this._apicards.getCards().subscribe(result=>{
+  //     this.cardList=result
+  //   })
+  //   console.log(this.cardList)
+  // }
+
+
 }
--- src/app/services/tarjeta.service.ts
+++ src/app/services/tarjeta.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class TarjetaService {
+export class CardsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  getCards(): Observable<any> {
+
+   
+    
+    const URL = 'http://localhost:1337/api/cards'
+    return this.http.get(URL)
+  }
 }
