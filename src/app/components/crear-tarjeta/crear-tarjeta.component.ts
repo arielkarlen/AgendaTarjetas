d5fcc1b61534cd070e121f2099354a3649caeb09
arie.karlen <ariel.karlen@possumus.tech>
2023-05-16 16:46:17 -0300

    FEATURE | Edit card

--- src/app/components/crear-tarjeta/crear-tarjeta.component.ts
+++ src/app/components/crear-tarjeta/crear-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TarjetaCredito } from 'src/app/models/tarjeta';
 import { CardsService } from 'src/app/services/tarjeta.service';
@@ -8,11 +8,30 @@ import { CardsService } from 'src/app/services/tarjeta.service';
   templateUrl: './crear-tarjeta.component.html',
   styleUrls: ['./crear-tarjeta.component.css']
 })
-export class CrearTarjetaComponent {
+export class CrearTarjetaComponent implements OnInit {
 
   forms: FormGroup;
 
- 
+  title ='Añadir '
+id = undefined
+
+  cardDataForEdit:any
+
+ ngOnInit(): void {
+   this.cardsService.getCard().subscribe(res=>{
+
+    this.cardDataForEdit=res;
+    this.title="Editar "
+    this.id=this.cardDataForEdit.id
+    this.forms.patchValue({
+      cvv: this.cardDataForEdit.attributes.cvv,
+      fechaExpiracion: this.cardDataForEdit.attributes.fechaExpiracion,
+      numeroDeTarjeta: this.cardDataForEdit.attributes.numeroDeTarjeta,
+      titular: this.cardDataForEdit.attributes.titular
+    })
+    console.log(this.id);
+   })
+ }
   
   constructor(private fb: FormBuilder, public cardsService: CardsService) {
 
@@ -23,6 +42,20 @@ export class CrearTarjetaComponent {
       fechaExpiracion: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
       cvv: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]],
     })
+  }
+
+
+  onSubmit() {
+    if(this.id == undefined)
+    {
+      this.crearTarjeta()
+     
+    } else{
+      this.editarTarjeta(this.id)
+      
+    }
+    window.location.reload();
+
   }
 
   crearTarjeta() {
@@ -37,7 +70,19 @@ export class CrearTarjetaComponent {
      }
 
     this.cardsService.createCard(card).subscribe()
-    
+  }
+
+  editarTarjeta(id:number) {
+    const card: TarjetaCredito =  {
+      data: {
+        titular: this.forms.value.titular,
+        numeroDeTarjeta: this.forms.value.numeroDeTarjeta,
+        fechaExpiracion: this.forms.value.fechaExpiracion,
+        cvv: this.forms.value.cvv,
+      }
+      
+     }
+      this.cardsService.editCard(id, card).subscribe()
   }
 
 }
--- src/app/models/tarjeta.ts
+++ src/app/models/tarjeta.ts
@@ -7,6 +7,7 @@ export class TarjetaCredito {
         fechaExpiracion:string;
         cvv: number;
     }
+
    
   
 
